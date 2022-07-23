e896137fb702b12c86a7d4fcf161d6b9150cc956
Moises Perez <pr20028@ues.edu.sv>
2022-07-23 17:04:47 -0600

    Se agrega div a p terrarium

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-window.alert("")
+let iceCreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Pistachio", "Rocky Road"];
+
+for (let i = 0; i < iceCreamFlavors.length; i++) {
+  console.log(iceCreamFlavors[i]);
+} //Ends when all flavors are printed
