c7f12ee0729fb1e9cafb4dde25881f029bb4c7d4
Moises Perez <moisestimoteop@gmail.com>
2022-07-23 17:27:57 -0600

    sw branch personal

--- index.js
+++
@@ -1,5 +0,0 @@
-let iceCreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Pistachio", "Rocky Road"];
-
-for (let i = 0; i < iceCreamFlavors.length; i++) {
-  console.log(iceCreamFlavors[i]);
-} //Ends when all flavors are printed
